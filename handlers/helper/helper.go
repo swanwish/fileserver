2dd2ecb5fd29e87c9e74aae16c77782621345ad0
Stephen <liwp.stephen@gmail.com>
2018-01-24 10:58:10 +0800

    fix index.html issue

--- handlers/helper/helper.go
+++ handlers/helper/helper.go
@@ -13,7 +13,10 @@ import (
 
 type FSDir string
 
+var indexFileNames = []string{"index.html", "index.htm"}
+
 func (d FSDir) Open(name string) (http.File, error) {
+	logs.Debugf("The name is %s", name)
 	if filepath.Separator != '/' && strings.ContainsRune(name, filepath.Separator) ||
 		strings.Contains(name, "\x00") {
 		return nil, errors.New("http: invalid character in file path")
@@ -23,13 +26,62 @@ func (d FSDir) Open(name string) (http.File, error) {
 		dir = "."
 	}
 	realPath := filepath.Join(dir, filepath.FromSlash(path.Clean("/"+name)))
-	if fileStat, err := os.Stat(realPath); err == nil && !fileStat.IsDir() {
-		f, err := os.Open(realPath)
-		if err != nil {
+	logs.Debugf("The real path is %s", realPath)
+	fileStat, err := os.Stat(realPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			logs.Errorf("File %s does not exists", realPath)
 			return nil, err
 		}
-		return f, nil
+		logs.Errorf("Failed to get status for file %s, the error is %#v", realPath, err)
+		return nil, err
+	}
+	// Check index file
+	if fileStat.IsDir() {
+		indexFilePath, err := getIndexFilePath(realPath)
+		if err != nil {
+			logs.Errorf("Failed to get index file path from dir %s, the error is %#v", realPath, err)
+			return nil, err
+		}
+		if name == "/" {
+			return getFileFromPath(realPath)
+		}
+		logs.Debugf("The index file path is %s", indexFilePath)
+		return getFileFromPath(indexFilePath)
+	}
+
+	if !fileStat.IsDir() {
+		return getFileFromPath(realPath)
 	}
 	logs.Errorf("The path %s is dir, which is not allowed", realPath)
 	return nil, os.ErrPermission
+}
+
+func getIndexFilePath(dirPath string) (string, error) {
+	logs.Debugf("Get index file path from dir %s", dirPath)
+	for _, indexFileName := range indexFileNames {
+		indexFilePath := filepath.Join(dirPath, indexFileName)
+		logs.Debugf("Check index file %s", indexFilePath)
+		osStat, err := os.Stat(indexFilePath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+			return "", err
+		}
+		if !osStat.IsDir() {
+			return indexFilePath, nil
+		}
+	}
+	return "", os.ErrNotExist
+}
+
+func getFileFromPath(realPath string) (http.File, error) {
+	f, err := os.Open(realPath)
+	//defer f.Close()
+	if err != nil {
+		logs.Errorf("Failed to open file %s, the error is %#v", realPath, err)
+		return nil, err
+	}
+	return f, nil
 }
